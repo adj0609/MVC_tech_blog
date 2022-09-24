864007e9ca2439e49ad4f26c91d1c2ab2abbe408
Andrea Jared <adj0803@gmail.com>
2022-09-24 05:43:15 -0400

    comment and post routes done, start of sign up in

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,57 @@
+const router = require('express').Router();
+const { brotliDecompress } = require('zlib');
+const { User, Post, Comment, Vote } = require('../../models');
+
+//GET all users
+router.get('/', (req, res) => {
+    User.findAll({
+       attributes: { exclude: ['password'] }
+    })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//GET ONE USER
+router.get('/:id', (req, res) => {
+    User.findOne({
+        attributes: { exclude: ['password'] },
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'title', 'post_url', 'created_at']
+            },
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'created_at'],
+                include: {
+                    model: Post,
+                    attributes: ['title']
+                }
+            },
+            {
+                model: Post,
+                attributes: ['title'],
+                through: Vote,
+                as: 'voted_posts'
+            }
+        ]
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found'});
+            return;
+        }
+        res.json(dbUserData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
